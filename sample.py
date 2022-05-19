fcc44c232a9460d165d27f6cd34732e87fadd2b4
blockenters <58394003+blockenters@users.noreply.github.com>
2022-05-19 16:42:25 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,3 +1,19 @@
+# 디렉토리 정보와 파일을 알려주면, 해당 디렉토리에
+# 파일을 저장하는 함수
+def save_uploaded_file(directory, file) :
+    # 1.디렉토리가 있는지 확인하여, 없으면 디렉토리부터만든다.
+    if not os.path.exists(directory) :
+        os.makedirs(directory)
+    # 2. 디렉토리가 있으니, 파일을 저장.
+    with open(os.path.join(directory, file.name), 'wb') as f :
+        f.write(file.getbuffer())
+    return st.success("Saved file : {} in {}".format(file.name, directory))
+
+
+
+--------
+
+
 # 문장 클리닝하는 파이프라이닝 함수
 
 import string
